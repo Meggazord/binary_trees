44fc7ed3bff9fbbf227827a1bcf5ca7a5040b632
Meggazord <k_megahed@hotmail.com>
2024-03-01 20:14:58 +0200

    xedf

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,28 +1,6 @@
 #include "binary_trees.h"
 
 size_t max_size(size_t x, size_t y);
-size_t binary_tree_height(const binary_tree_t *tree);
-
-/**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree
- * Return: Balance factor, 0 if tree is NULL
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-    binary_tree_t *left, *right;
-
-    left = NULL;
-    right = NULL;
-
-    if (tree == NULL)
-        return (0);
-
-    left = tree->left;
-    right = tree->right;
-
-    return (binary_tree_height(left) - binary_tree_height(right));
-}
 
 /**
  * binary_tree_height - Measures the height of a binary tree
@@ -61,4 +39,27 @@ size_t max_size(int x, int y)
 {
     return (x >= y ? x : y);
 }
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: Balance factor, 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    binary_tree_t *left, *right;
+
+    left = NULL;
+    right = NULL;
+
+    if (tree == NULL)
+        return (0);
+
+    left = tree->left;
+    right = tree->right;
+
+    return (binary_tree_height(left) - binary_tree_height(right));
+}
+
+
 
