d2e968b632115f794d0c91e492af285c5ea9fefb
Meggazord <k_megahed@hotmail.com>
2024-03-01 20:25:24 +0200

    xiedG

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,21 +7,86 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    size_t height_left, height_right;
+    binary_tree_t *left, *right;
+
+    left = NULL;
+    right = NULL;
 
     if (tree == NULL)
         return (0);
 
-    height_left = binary_tree_height(tree->left);
-    height_right = binary_tree_height(tree->right);
+    if ((tree->left == NULL) && (tree->right == NULL))
+        return (1);
 
-    if (height_left == height_right)
-    {
-        if (tree->left == NULL && tree->right == NULL)
-            return (1);
+    if (tree->left == NULL ^ tree->right == NULL)
+        return (0);
 
-        return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
-    }
+    if (binary_tree_balance(tree) != 0)
+        return (0);
 
-    return (0);
+    left = tree->left;
+    right = tree->right;
+
+    return (1 && binary_tree_is_perfect(left) && binary_tree_is_perfect(right));
+
 }
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: Balance factor, 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    binary_tree_t *left, *right;
+
+    left = NULL;
+    right = NULL;
+
+    if (tree == NULL)
+        return (0);
+
+    left = tree->left;
+    right = tree->right;
+
+    return (binary_tree_height(left) - binary_tree_height(right));
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: The height of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    binary_tree_t *left, *right;
+
+    left = NULL;
+    right = NULL;
+
+    if (tree == NULL)
+        return (0);
+
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+
+    left = tree->left;
+    right = tree->right;
+
+    return (max_size(binary_tree_height(left), binary_tree_height(right)) + 1);
+}
+
+
+/**
+* max_size - return the max of 2 no.s
+* @x: 1st no
+* @y: 2nd no
+*
+* Return: max of 2 numbers
+*/
+size_t max_size(int x, int y)
+{
+    return (x >= y ? x : y);
+}
+
