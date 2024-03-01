2bb08745aee4f93c1a666c1af5df057e13950953
Meggazord <k_megahed@hotmail.com>
2024-03-01 20:02:35 +0200

    fixed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,13 +7,16 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+    binary_tree_t *left, *right;
+
+    left = NULL;
+    right = NULL;
 
     if (tree == NULL)
         return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+    left = tree->left;
+    right tree->right;
 
-    return (left_height - right_height);
+    return (binary_tree_height(left) - binary_tree_height(right));
 }
