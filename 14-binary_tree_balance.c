ee9cd7111ac575aba0af21103e2e7ee5edafdb42
Meggazord <k_megahed@hotmail.com>
2024-03-01 20:16:32 +0200

    doxed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -38,7 +38,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
         return (0);
 
     if (tree->left == NULL && tree->right == NULL)
-        return (0);
+        return (1);
 
     left = tree->left;
     right = tree->right;
