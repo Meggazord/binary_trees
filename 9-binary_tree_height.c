e5956f8ab41986835e567af2a4b4c248752b33ae
Meggazord <k_megahed@hotmail.com>
2024-03-01 19:16:58 +0200

    fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,6 +13,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
     if (tree == NULL)
         return (0);
 
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+
     height_l = binary_tree_height(tree->left);
     height_r = binary_tree_height(tree->right);
 
