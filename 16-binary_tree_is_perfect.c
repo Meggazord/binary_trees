d046f3d92188f1e9652a7607724865ee782b0456
Meggazord <k_megahed@hotmail.com>
2024-03-01 20:27:04 +0200

    :dixEd

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,11 +15,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
     if (tree == NULL)
         return (0);
 
-    if ((tree->left == NULL) && (tree->right == NULL))
-        return (1);
-
     if (tree->left == NULL ^ tree->right == NULL)
         return (0);
+
+    if ((tree->left == NULL) && (tree->right == NULL))
+        return (1);
 
     if (binary_tree_balance(tree) != 0)
         return (0);
